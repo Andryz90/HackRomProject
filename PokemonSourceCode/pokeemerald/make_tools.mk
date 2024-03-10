272d052b38f565470642d833b6a2fa2990ff566f
Andry <andrea.grieco@hotmail.com>
2024-03-10 09:54:18 +0100

    Update from pokeemerald to pokeemerald-expansion with all the new Pokemon and gen 9 meccanics (see log file) Added poryscrit enviroment to tools to create and edits script for the game

---
+++ PokemonSourceCode/pokeemerald/make_tools.mk
@@ -0,0 +1,11 @@
+MAKEFLAGS += --no-print-directory
+
+# Inclusive list. If you don't want a tool to be built, don't add it here.
+TOOLDIRS := $(filter-out tools/agbcc tools/binutils tools poryscript, $(wildcard tools/*))
+
+.PHONY: all $(TOOLDIRS)
+
+all: $(TOOLDIRS)
+
+$(TOOLDIRS):
+	@$(MAKE) -C $@
